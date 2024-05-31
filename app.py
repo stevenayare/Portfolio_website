3c3661bce0bf6af7d4e84ab9680a5320d7a61446
Ved <64102456+stevenayare@users.noreply.github.com>
2024-05-31 21:45:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from PIL import Image
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "Ved_latest.pdf"
-profile_pic = current_dir / "assets" / "Ved.JPG"
+profile_pic = current_dir / "assets" / "Ved.jpg"
 
 # General settings
 PAGE_TITLE = "Digital CV | Ved Ayare"
